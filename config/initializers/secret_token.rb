f9a89303aee47d598b8f62b8917019b8a8f051d9
James Cox <th3james@gmail.com>
2011-12-07 20:17:09 +0000

    Basic editing working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoteMark::Application.config.secret_token = 'd170e6eae9776abba5aaffc3e691b303446b7555a8329f16a9d26b9f8ccc17a41ddf510e7c6f5c7ba05986b5a5336d95ee4f00dc53007056037d944bb03fc9cd'

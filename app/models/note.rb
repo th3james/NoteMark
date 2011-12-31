2ff14d3584166b841d02eda2c9a9285e52946646
James Cox <th3james@gmail.com>
2011-12-31 16:12:04 +0000

    Added some lovelier lorem ipsum

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,12 +1,15 @@
 class Note < ActiveRecord::Base
   DEFAULT_TITLE = 'Note Name...'
-  DEFAULT_BODY = "# Heading
-Some text
+  DEFAULT_BODY = "# Neque porro quisquam est qui dolorem ipsum
+Fusce eu arcu velit. Sed lobortis quam vel velit gravida sagittis. In a eros quis quam sagittis ullamcorper non id neque. Cras eros elit, elementum sed ullamcorper quis, blandit vitae lacus. Sed vel quam neque. In hac habitasse platea dictumst. Etiam tincidunt risus non sem egestas sit amet bibendum mauris fringilla.
 
-* bullet
-* point
+* Praesent velit eros.
+* Praesent elementum ultricies nisi.
 
-## Sub head"
+### Morbi lobortis varius imperdiet
+
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec blandit erat suscipit felis ultricies interdum vel a urna. Duis vitae elit vulputate libero malesuada mollis sit amet non lacus. Aenean pulvinar mollis facilisis. Morbi lobortis varius imperdiet. Pellentesque interdum nisl quis enim eleifend non luctus elit egestas. Aliquam eu sagittis massa.
+"
   
   # Initialises a new note with default values
   #
